d144e479f71d6bcaefcae0eae1bbc0d42b222c50
gareth <danger@alphashack.com>
2012-07-16 10:27:55 +1000

    rooms route added

--- services/index.go
+++ services/index.go
@@ -12,29 +12,40 @@ type Message struct {
 	Id int64
 }
 
+func getMessage() Message {
+	return Message{
+		Name: "Joss whedon's fan club",
+		Id:	1,
+	}
+}
+
+func toJson(item interface{}) []byte {
+	b, err := json.Marshal(item)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return b;
+}
+
 func main() {
-	/*
-	web.Get("/rooms$", func(ctx * web.Context) {
+	web.Get("/rooms", func(ctx * web.Context) {
 		ctx.SetHeader("Content-Type", "application/json", true);
-		ctx.WriteString("[]");
+		messages := []Message{getMessage()}
+		fmt.Println(messages);
+
+		response := toJson(messages);
+		ctx.Write(response);
 	});
-	*/
 
 	web.Get("/rooms/([0-9]+)", func(ctx * web.Context, val string) {
 		ctx.SetHeader("Content-Type", "application/json", true);
 		id,_ := strconv.ParseInt(val, 0, 64);
 
-		message := Message{
-			Name: "/rooms",
-			Id:	id,
-		}
+		message := getMessage();
+		message.Id = id;
+		response := toJson(message);
 
-		b, err := json.Marshal(message)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-
-		ctx.Write(b);
+		ctx.Write(response);
 	});
 
     web.Run("0.0.0.0:9999")
