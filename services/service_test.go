6302e10b158a25735d3cb999d223815b838bd244
gareth <danger@alphashack.com>
2012-07-24 22:48:12 +1000

    first test motherfucker!!

--- services/service_test.go
+++ services/service_test.go
@@ -6,6 +6,7 @@ import (
 	"web"
 	"net/http"
 	"net/http/httptest"
+	"encoding/json"
 )
 
 //initialize the routes
@@ -13,11 +14,30 @@ func init() {
 	RegisterRoutes();
 }
 
-func TestHelloWorld(t * testing.T) {
+func TestLogin(t * testing.T) {
+	// Arrange
 	recorder := httptest.NewRecorder()
 	request, _ := http.NewRequest("POST", "/login", nil)
 
+	// Act
 	web.AdHoc(recorder, request)
 
-	fmt.Println("Result", recorder.Body)
+	// Assert
+	user := new(User)
+	json.Unmarshal(recorder.Body.Bytes(), user)
+
+	if user.Name != "Malcom Renyolds" {
+		t.Error("Incorrect name returned");
+	}
+
+	if user.Email != "mal@serenity.com" {
+		t.Error("Incorrect email returned");
+	}
+
+	if user.Id != 1 {
+		t.Error("Incorrect id returned");
+	}
+
+
+	fmt.Println("Result", user)
 }
